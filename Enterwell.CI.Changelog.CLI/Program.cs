c4370296f7b73dae851760b971d7cc6bb529f798
Dorijan <dorijan.haspl@enterwell.net>
2021-02-15 15:37:18 +0100

    CLI project setup

---
+++ Enterwell.CI.Changelog.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Enterwell.CI.Changelog.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
