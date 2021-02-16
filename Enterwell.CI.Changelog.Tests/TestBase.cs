056328632e01a1a7294d197de8bcf44aeddf0807
Dorijan <dorijan.haspl@enterwell.net>
2021-02-16 11:55:45 +0100

    Added comments in the Tests project.

--- Enterwell.CI.Changelog.Tests/TestBase.cs
+++ Enterwell.CI.Changelog.Tests/TestBase.cs
@@ -3,18 +3,50 @@ using System.IO;
 
 namespace Enterwell.CI.Changelog.Tests
 {
+    /// <summary>
+    /// Base class for our tests. Contains logic to help setup almost every test, by calling its methods in the Arrange part of the test. (Following AAA pattern).
+    /// By inheriting the <see cref="IDisposable"/> and implementing its <see cref="Dispose"/> method, we can designate a method to run after every test.
+    /// </summary>
     public class TestBase : IDisposable
     {
+        /// <summary>
+        /// Gets the path to the system Temp directory which we are using for testing purposes.
+        /// </summary>
         protected readonly string TemporaryDirectoryPath = Path.GetTempPath();
+        
+        /// <summary>
+        /// Name of the folder which is going to be created inside system Temp directory for testing purposes.
+        /// </summary>
         protected readonly string TestFolderName = "Testing";
+
+        /// <summary>
+        /// Name of the folder inside the Testing folder which contains all of the changes with which our program works. Needs to be 'changes'.
+        /// </summary>
         protected readonly string ChangesFolderName = "changes";
 
+        /// <summary>
+        /// Name of the configuration file we are using to configure Changelog program.
+        /// </summary>
         protected readonly string ChangelogFileName = "Changelog.md";
-        
+
+        /// <summary>
+        /// Path got by combining the path to the system Temp directory with <see cref="TestFolderName"/>
+        /// </summary>
         protected readonly string TestFolderPath;
+
+        /// <summary>
+        /// Path got by combining the path to the system Temp directory with <see cref="ChangesFolderName"/>
+        /// </summary>
         protected readonly string ChangesFolderPath;
+
+        /// <summary>
+        /// Path got by combining the path to the system Temp directory with <see cref="ChangelogFileName"/>
+        /// </summary>
         protected readonly string ChangelogFilePath;
 
+        /// <summary>
+        /// <see cref="TestBase"/> constructor. It runs before every test in the test class that inherit the <see cref="TestBase"/>.
+        /// </summary>
         public TestBase()
         {
             TestFolderPath = Path.Combine(TemporaryDirectoryPath, TestFolderName);
@@ -24,6 +56,11 @@ namespace Enterwell.CI.Changelog.Tests
             Directory.CreateDirectory(TestFolderPath);
         }
 
+        /// <summary>
+        /// Creates a directory on a given path. If the path starts with anything other than <see cref="TestFolderPath"/>, exception is thrown.
+        /// </summary>
+        /// <param name="folderPath">Path to the directory that needs to be created.</param>
+        /// <exception cref="Exception">If the path starts with anything other than <see cref="TestFolderPath"/>.</exception>
         protected void CreateDirectory(string folderPath)
         {
             if (!folderPath.StartsWith(TestFolderPath))
@@ -34,6 +71,11 @@ namespace Enterwell.CI.Changelog.Tests
             Directory.CreateDirectory(folderPath);
         }
 
+        /// <summary>
+        /// Creates a file on a given path. If the path starts with anything other than <see cref="TestFolderPath"/>, exception is thrown.
+        /// </summary>
+        /// <param name="filePath">Path to the file that needs to be created.</param>
+        /// <exception cref="Exception">If the path starts with anything other than <see cref="TestFolderPath"/>.</exception>
         protected void CreateFile(string filePath)
         {
             if (!filePath.StartsWith(TestFolderPath))
@@ -45,11 +87,20 @@ namespace Enterwell.CI.Changelog.Tests
             file.Close();
         }
 
+        /// <summary>
+        /// Writes arbitrary content to the file on a given path.
+        /// </summary>
+        /// <param name="filePath">Path to the file that needs to be written to.</param>
+        /// <param name="text">Content that will be written to the file.</param>
         protected void WriteToFile(string filePath, string text)
         {
             File.WriteAllText(filePath, text);
         }
 
+        /// <summary>
+        /// Creates directory on a <see cref="ChangesFolderPath"/> path and fills it with various files
+        /// containing different names that could be an edge-case for our application.
+        /// </summary>
         protected void CreateChanges()
         {
             CreateDirectory(ChangesFolderPath);
@@ -80,6 +131,9 @@ namespace Enterwell.CI.Changelog.Tests
             }
         }
 
+        /// <summary>
+        /// Creates a <see cref="ChangelogFileName"/> file on a <see cref="ChangelogFilePath"/> path and fills it with some initial data.
+        /// </summary>
         protected void CreateChangelog()
         {
             CreateFile(ChangelogFilePath);
@@ -98,6 +152,9 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
             WriteToFile(ChangelogFilePath, changelogContent);
         }
 
+        /// <summary>
+        /// Clears the <see cref="TestFolderPath"/> from the system Temp that was used for testing. It is ran after every test in our test suite.
+        /// </summary>
         public void Dispose()
         {
             Directory.Delete(TestFolderPath, true);
