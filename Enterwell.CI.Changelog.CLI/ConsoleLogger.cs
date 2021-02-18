5f3a49a559af6b02cf7cbffb927ef66533ba2eb6
Dorijan <dorijan.haspl@enterwell.net>
2021-02-18 10:48:25 +0100

    Documented ConsoleLogger class.

--- Enterwell.CI.Changelog.CLI/ConsoleLogger.cs
+++ Enterwell.CI.Changelog.CLI/ConsoleLogger.cs
@@ -2,8 +2,16 @@
 
 namespace Enterwell.CI.Changelog.CLI
 {
+    /// <summary>
+    /// Class used to log to the console with appropriate foreground colors.
+    /// </summary>
     public class ConsoleLogger
     {
+        /// <summary>
+        /// Uses the console to notify the user if the change file was successfully created and added. 
+        /// </summary>
+        /// <param name="changeCreated"><see cref="bool"/> that is used to determine if the file was created or not.</param>
+        /// <param name="reason"><see cref="string"/> that specifies the reason if the file was not created.</param>
         public void LogResult(bool changeCreated, string reason)
         {
             if (!changeCreated)
@@ -16,6 +24,10 @@ namespace Enterwell.CI.Changelog.CLI
             }
         }
 
+        /// <summary>
+        /// Logs an error to the console using the appropriate foreground color and shows how to look for help in this CLI application.
+        /// </summary>
+        /// <param name="statusText"><see cref="string"/> to log to the console.</param>
         public void LogError(string statusText)
         {
             Console.ForegroundColor = ConsoleColor.Red;
@@ -24,6 +36,10 @@ namespace Enterwell.CI.Changelog.CLI
             Console.Error.WriteLine("Specify --help for a list of available options and commands");
         }
 
+        /// <summary>
+        /// Logs a success to the console using the appropriate foreground color.
+        /// </summary>
+        /// <param name="statusText"><see cref="string"/> to log to the console.</param>
         private void LogSuccess(string statusText)
         {
             Console.ForegroundColor = ConsoleColor.Green;
