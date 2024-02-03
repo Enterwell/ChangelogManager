70e6cc886af1a4035738b6415355ab3f4b7cef83
DorijanH <dhaspl@gmail.com>
2024-02-03 03:28:00 +0100

    feat: Added VSCode extension initial version

---
+++ Enterwell.CI.Changelog.VSCodeExtension/src/constants.ts
@@ -0,0 +1,4 @@
+/**
+ * Allowed types of changes. Types following the 'Keep a Changelog' guiding principles.
+ */
+export const allowedTypes: string[] = ['Added', 'Changed', 'Deprecated', 'Removed', 'Fixed', 'Security'];
