b67698faf172ddd28d0ab08e5c94fca4b9b72de6
DorijanH <dhaspl@gmail.com>
2025-04-09 12:30:03 +0200

    feat(vscode-extension): Traversing upwards to find the nearest CHANGELOG file

--- Enterwell.CI.Changelog.VSCodeExtension/src/constants.ts
+++ Enterwell.CI.Changelog.VSCodeExtension/src/constants.ts
@@ -4,6 +4,11 @@
 export const allowedTypes: string[] = ['Added', 'Changed', 'Deprecated', 'Removed', 'Fixed', 'Security'];
 
 /**
+ * Changelog name.
+ */
+export const changelogName: string = 'CHANGELOG.md';
+
+/**
  * Changelog configuration name.
  */
 export const configurationName: string = '.changelog.json';
